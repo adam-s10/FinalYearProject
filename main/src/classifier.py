0723171c80b2e2a68a96bc4b9377e856557db26c
Adam <adam@steinmetz.com>
2025-03-05 14:19:16 +0000

    classifier.py - Removed redundant cross_validation() function - Removed redundant calculate_stats() function - Removed create_meta_dataset_old() function - Removed classify_metafile_old() - Removed redundant classify_dataset()

--- main/src/classifier.py
+++ main/src/classifier.py
@@ -303,33 +303,6 @@ def run_all_classifiers():
     print("number of errors --> ", error_count)
 
 
-# Cross validation method
-# TODO: Remove duplicate code - remove by 03/01/25
-def cross_validation(classifier, data, classes, classifier_name, file):
-    logger.info(f"cross_validation: Classification for {file} using classifier {classifier_name} starting:")
-    data = data.to_numpy()  # convert to numpy array
-    classes = classes.values.tolist()  # convert to list
-    acc = []  # create list to store accuracies
-    skf = StratifiedKFold(n_splits=10, shuffle=True)  # 10-fold split with data shuffle
-    skf.get_n_splits(data, classes)  # get the splits using skf
-    for train_index, test_index in skf.split(data, classes):  # split the data
-        # get train and test data
-        train_d = data[train_index]
-        train_c = [classes[j] for j in train_index]
-        test_d = data[test_index]
-        test_c = [classes[j] for j in test_index]
-
-        classifier.fit(train_d, train_c)  # classify
-        # Evaluate model on testing data
-        y_pred = classifier.predict(test_d)
-        a = accuracy_score(y_pred, test_c)  # get accuracy
-        acc.append(a)  # add accuracy to list
-    logger.info(f"cross_validation: Cross validation scores for {file} using classifier {classifier_name} --> {acc}")
-    logger.info(f"cross_validation: Average --> {statistics.mean(acc)}")
-    logger.info(f"cross_validation: Number of CV Scores used in Average --> {len(acc)}")
-    return acc
-
-
 # Returns the features and targets of the provided dataset
 def preprocess_data(path, file):
     logger.info(f"preprocess_data: Starting for file --> {file}")
@@ -340,17 +313,6 @@ def preprocess_data(path, file):
     classes = dataset.iloc[:, -1:]  # Only the last column, targets
     logger.info(f"preprocess_data: Finished for file --> {file}")
     return dataset, data, classes
-
-
-# Returns standard deviation, mean, maximum, minimum of the provided accuracy
-# TODO: Verify functionality remains the same
-# TODO: Remove duplicate code - remove by 03/01/25
-def calculate_stats(acc):
-    standard_deviation = statistics.stdev(acc)
-    mean = statistics.mean(acc)
-    maximum = max(acc)
-    minimum = min(acc)
-    return standard_deviation, mean, maximum, minimum
 
 
 # Creates the meta-dataset to make predictions on what the best classifier would be for a given dataset
@@ -430,91 +392,6 @@ def create_meta_dataset():
         shutil.move(f"{path_to_csvs}/{file}", f"D:/PycharmProjects/FinalYearProject/completedDatasets/{file}")
 
 
-# TODO: verify functionality remains the same - Done
-# TODO: remove duplicate code
-def create_meta_dataset_old():
-    error_count = 0
-    for file in os.listdir("D:/PycharmProjects/FinalYearProject/csvDatasets"):
-        dataset, data, classes = preprocess_data(path_to_csvs, file)
-        rows = dataset.shape[0]  # Get rows using pandas
-        columns = dataset.shape[1]  # Get columns using pandas
-
-        # Classify for Support Vector Machine
-        try:
-            svm_classifier = svm.SVC()
-            svm_acc = cross_validation(svm_classifier, data, classes, "SVM", file)
-        except ValueError:  # If fails; skip
-            logger.error(f"{file} raised value error, skipping")
-            shutil.move(f"{path_to_csvs}/{file}", f"D:/PycharmProjects/FinalYearProject/inconsistentFailures/{file}")
-            error_count += 1
-            continue
-        svm_sd, svm_mean, svm_max, svm_min = calculate_stats(svm_acc)
-        logger.info(f"create_meta_dataset: SVM returned mean --> {svm_mean}")
-        logger.info(f"create_meta_dataset: SVM returned standard deviation --> {svm_sd}")
-        logger.info(f"create_meta_dataset: SVM returned minimum --> {svm_min}")
-        logger.info(f"create_meta_dataset: SVM returned maximum --> {svm_max}")
-
-        # Classify for neural network
-        nn_classifier = MLPClassifier(max_iter=500)
-        nn_acc = cross_validation(nn_classifier, data, classes, "NN", file)
-        nn_sd, nn_mean, nn_max, nn_min = calculate_stats(nn_acc)
-        logger.info(f"create_meta_dataset: NN returned mean --> {nn_mean}")
-        logger.info(f"create_meta_dataset: NN returned standard deviation --> {nn_sd}")
-        logger.info(f"create_meta_dataset: NN returned minimum --> {nn_min}")
-        logger.info(f"create_meta_dataset: NN returned maximum --> {nn_max}")
-
-        # Classify for random forrest
-        rf_classifier = RandomForestClassifier()
-        rf_acc = cross_validation(rf_classifier, data, classes, "RF", file)
-        rf_sd, rf_mean, rf_max, rf_min = calculate_stats(rf_acc)
-        logger.info(f"create_meta_dataset: RF returned mean --> {rf_mean}")
-        logger.info(f"create_meta_dataset: RF returned standard deviation --> {rf_sd}")
-        logger.info(f"create_meta_dataset: RF returned minimum --> {rf_min}")
-        logger.info(f"create_meta_dataset: RF returned maximum --> {rf_max}")
-
-        # Classify for Logistic Regression
-        lr_classifier = LogisticRegression()
-        lr_acc = cross_validation(lr_classifier, data, classes, "LR", file)
-        lr_sd, lr_mean, lr_max, lr_min = calculate_stats(lr_acc)
-        logger.info(f"create_meta_dataset: LR returned mean --> {lr_mean}")
-        logger.info(f"create_meta_dataset: LR returned standard deviation --> {lr_sd}")
-        logger.info(f"create_meta_dataset: LR returned minimum --> {lr_min}")
-        logger.info(f"create_meta_dataset: LR returned maximum --> {lr_max}")
-
-        # Classify for Naive Bayes
-        nb_classifier = GaussianNB()
-        nb_acc = cross_validation(nb_classifier, data, classes, "NB", file)
-        nb_sd, nb_mean, nb_max, nb_min = calculate_stats(nb_acc)
-        logger.info(f"create_meta_dataset: NB returned mean --> {nb_mean}")
-        logger.info(f"create_meta_dataset: NB returned standard deviation --> {nb_sd}")
-        logger.info(f"create_meta_dataset: NB returned minimum --> {nb_min}")
-        logger.info(f"create_meta_dataset: NB returned maximum --> {nb_max}")
-
-        # ---Get the best classifier using mean---
-        # dictionary containing means
-        var = {svm_mean: "svm", nn_mean: "nn", rf_mean: "rf", lr_mean: "lr", nb_mean: "nb"}
-        switch_case = var.get(max(var))  # get the value with the highest mean
-        match switch_case:  # write to file depending on what was the best classifier
-            case "svm":
-                best_classifier = 1
-                write_meta_data(file, columns, rows, svm_min, svm_max, svm_sd, svm_mean, best_classifier)
-            case "nn":
-                best_classifier = 2
-                write_meta_data(file, columns, rows, nn_min, nn_max, nn_sd, nn_mean, best_classifier)
-            case "rf":
-                best_classifier = 3
-                write_meta_data(file, columns, rows, rf_min, rf_max, rf_sd, rf_mean, best_classifier)
-            case "lr":
-                best_classifier = 4
-                write_meta_data(file, columns, rows, lr_min, lr_max, lr_sd, lr_mean, best_classifier)
-            case "nb":
-                best_classifier = 5
-                write_meta_data(file, columns, rows, nb_min, nb_max, nb_sd, nb_mean, best_classifier)
-
-        # Once all 5 classifiers are complete move the dataset to new folder
-        shutil.move(f"{path_to_csvs}/{file}", f"D:/PycharmProjects/FinalYearProject/completedDatasets/{file}")
-
-
 # Predict the best classifier using created meta-dataset
 def classify_metafile():
     file = "metadata_file4.csv"
@@ -556,66 +433,6 @@ def classify_metafile():
 
         f.write(f'Naive Bayes cross-validation results: {nb_classifier.accuracies}\n')
         f.write(f'Min: {nb_min}, Max: {nb_max}, Standard Deviation: {nb_sd}, Mean: {nb_mean}\n')
-
-
-# TODO: verify functionality remains the same - Done
-# TODO: remove duplicate code - remove by 03/05/25
-def classify_metafile_old():
-    file = "final_metadata_file.csv"
-    path_to_meta = "D:/PycharmProjects/FinalYearProject/MetaDataFiles"
-    f = open("D:/PycharmProjects/FinalYearProject/MetaDataFiles/results_of_meta_file.txt", "a")
-    data, a, b = preprocess_data(path_to_meta, file)
-
-    svm_results, nn_results, rf_results, lr_results, nb_results = classify_dataset(file, a, b)
-    svm_sd, svm_mean, svm_max, svm_min = calculate_stats(svm_results)
-    f.write(f"SVM Cross-validation: {svm_results}\n")
-    f.write(f"Min: {svm_min}, Max: {svm_max}, Standard Deviation: {svm_sd}, Mean: {svm_mean}\n")
-
-    nn_sd, nn_mean, nn_max, nn_min = calculate_stats(nn_results)
-    f.write(f"NN Cross-validation: {nn_results}\n")
-    f.write(f"Min: {nn_min}, Max: {nn_max}, Standard Deviation: {nn_sd}, Mean: {nn_mean}\n")
-
-    rf_sd, rf_mean, rf_max, rf_min = calculate_stats(rf_results)
-    f.write(f"RF Cross-validation: {rf_results}\n")
-    f.write(f"Min: {rf_min}, Max: {rf_max}, Standard Deviation: {rf_sd}, Mean: {rf_mean}\n")
-
-    lr_sd, lr_mean, lr_max, lr_min = calculate_stats(lr_results)
-    f.write(f"LR Cross-validation: {lr_results}\n")
-    f.write(f"Min: {lr_min}, Max: {lr_max}, Standard Deviation: {lr_sd}, Mean: {lr_mean}\n")
-
-    nb_sd, nb_mean, nb_max, nb_min = calculate_stats(nb_results)
-    f.write(f"NB Cross-validation: {nb_results}\n")
-    f.write(f"Min: {nb_min}, Max: {nb_max}, Standard Deviation: {nb_sd}, Mean: {nb_mean}\n")
-    f.close()
-
-
-# TODO: remove redundant code once classify_meta_file_old() functionality confirmed
-# runs all the classifiers returning their accuracy scores for 10-fold cross-validation
-def classify_dataset(file, a, b):
-    logger.info(f"run_all_classifiers: Beginning for file --> {file}")
-
-    logger.info(f"run_all_classifiers: SVM Starting")
-    svm_results = cross_validation(svm.SVC(), a, b, "SVM", file)
-    logger.info(f"run_all_classifiers: SVM Finished")
-
-    logger.info(f"run_all_classifiers: NN Starting")
-    nn_results = cross_validation(MLPClassifier(max_iter=500), a, b, "NN", file)
-    logger.info(f"run_all_classifiers: NN Finished")
-
-    logger.info(f"run_all_classifiers: RF Starting")
-    rf_results = cross_validation(RandomForestClassifier(), a, b, "RF", file)
-    logger.info(f"run_all_classifiers: RF Finished")
-
-    logger.info(f"run_all_classifiers: LR Starting")
-    lr_results = cross_validation(LogisticRegression(), a, b, "LR", file)
-    logger.info(f"run_all_classifiers: LR Finished")
-
-    logger.info(f"run_all_classifiers: NB Starting")
-    nb_results = cross_validation(GaussianNB(), a, b, "NB", file)
-    logger.info(f"run_all_classifiers: NB Finished")
-
-    logger.info(f"run_all_classifiers: Finished for file --> {file}")
-    return svm_results, nn_results, rf_results, lr_results, nb_results
 
 
 # move_invalid_datasets()
